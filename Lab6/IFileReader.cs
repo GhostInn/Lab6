d44157cde280c57de30e09a24fe2a2a6a7496ff8
GhostInn <112103443+GhostInn@users.noreply.github.com>
2024-06-01 00:02:40 +0300

    Update IFileReader.cs

--- Lab6/IFileReader.cs
+++ Lab6/IFileReader.cs
@@ -2,7 +2,5 @@ namespace Lab6;
 
 public interface IFileReader<T>
 {
-    // Этот метод должен реализовываться в классах, реализующих интерфейс IFileReader<T>.
-    // Он предназначен для чтения файла по указанному пути и возврата последовательности объектов типа T.
     IEnumerable<T> Read(string filePath);
 }
