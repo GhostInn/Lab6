3f38a56d6663ffe46d0280e3cdf39811b11d15b5
GhostInn <112103443+GhostInn@users.noreply.github.com>
2024-06-01 00:03:47 +0300

    Update Person.cs

--- Lab6/Person.cs
+++ Lab6/Person.cs
@@ -2,9 +2,7 @@ namespace Lab6;
 
 public class Person
 {
-    // Это свойство содержит имя человека.
     public string Name { get; set; }
-
-    // Это свойство содержит возраст человека.
+    
     public int Age { get; set; }
 }
